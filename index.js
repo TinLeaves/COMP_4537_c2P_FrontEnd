0ebfad1fad1426e681717e2c797c22d2bc78510e
Andy Lau <andyhplau@gmail.com>
2024-11-25 18:15:57 -0800

    move views and js folders into public

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const cookieParser = require("cookie-parser");
 
 // app.use("/views", express.static(path.join(__dirname, "views")));
 // app.use("/js", express.static(path.join(__dirname, "js")));
-app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 // app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
