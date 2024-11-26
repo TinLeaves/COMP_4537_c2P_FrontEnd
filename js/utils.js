51da2be41f069e44b59463c285bbcc321b5aadb1
TinLeaves <tinleaves.0@gmail.com>
2024-11-25 18:16:13 -0800

    added ui for login and register

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,18 @@ function goBack() {
 function goHome() {
     window.location.href = '/home';
 }
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('togglePassword');
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+}
